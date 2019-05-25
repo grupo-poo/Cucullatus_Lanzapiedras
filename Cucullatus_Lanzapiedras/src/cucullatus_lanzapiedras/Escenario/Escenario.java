589dd6e7f402d77406e44b88c7954a6ad71caf2c
Milton Lenis <mlenism@unal.edu.co>
2019-05-25 17:19:24 -0500

    Iniciamos

---
+++ Cucullatus_Lanzapiedras/src/cucullatus_lanzapiedras/Escenario/Escenario.java
@@ -0,0 +1,11 @@
+package cucullatus_lanzapiedras.Escenario;
+
+/**
+ * @author Nicolas Hoyos
+ * @author Nicolas Mejia
+ * @author Milton Lenis
+ * @author Diego Carvajal
+ */
+public class Escenario {
+    
+}
